462d39ccb33ed45d7f9faaae6d3080e930e2618e
Debanjan Konar <91771502+Uni-Bo@users.noreply.github.com>
2023-11-29 00:27:19 +0530

    Update Prime.java

--- oops/Prime.java
+++ oops/Prime.java
@@ -1,46 +1,57 @@
-public class Prime {
+class MyTh implements Runnable {
+    private Thread th;
+    private int start;
+    private int end;
 
-    private static int count = 0;
+    public MyTh(int start, int end) {
+        this.start = start;
+        this.end = end;
+        this.th = new Thread(this);
+    }
 
-    class MyTh implements Runnable {
-        private Thread th;
-        private int start;
-        private int end;
+    public void startThread() {
+        th.start();
+    }
 
-        public MyTh(int start, int end) {
-            this.start = start;
-            this.end = end;
-            th = new Thread(this);
-            th.start();
-        }
-
-        public void run() {
-            try {
-                System.out.println("Thread " + th.getId() + " running");
-                for (int i = start; i <= end; i++) {
-                    if (isPrime(i)) {
-                        synchronized (Prime.class) {
-                            count++;
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                System.out.println("Thread interrupted");
-            }
-        }
-
-        private boolean isPrime(int num) {
-            if (num < 2) {
-                return false;
-            }
-            for (int i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) {
-                    return false;
-                }
-            }
-            return true;
+    public void end()
+    {
+        try{
+            th.join();
+        }catch (InterruptedException e) {
+                e.printStackTrace();
         }
     }
+
+    public void run() {
+        try {
+            System.out.println("Thread " + th.getId() + " running");
+            for (int i = start; i <= end; i++) {
+                if (isPrime(i)) {
+                    synchronized (Prime.class) {
+                        Prime.count++;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Thread interrupted");
+        }
+    }
+
+    private boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+public class Prime {
+    public static int count = 0;
 
     public static void main(String[] args) {
         int n = 10000;
@@ -48,21 +59,21 @@ public class Prime {
 
         MyTh[] mt = new MyTh[no_of_th];
 
-        int rangePerThread = n / no_of_th;
+        int rangePerThread = 100;
         int startRange = 0;
         int endRange = rangePerThread - 1;
 
         for (int i = 0; i < no_of_th; i++) {
-            mt[i] = new Prime().new MyTh(startRange, endRange);
+            mt[i] = new MyTh(startRange, endRange);
+            mt[i].startThread();
             startRange = endRange + 1;
             endRange = (i == no_of_th - 2) ? n - 1 : endRange + rangePerThread;
         }
 
-        // Wait for all threads to finish
         for (MyTh thread : mt) {
             try {
-                thread.th.join();
-            } catch (InterruptedException e) {
+                thread.end();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
